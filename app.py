04c274af8fc6182b0511d1fc23bfdb315c014a95
namonroyr <namonroyr@unal.edu.co>
2021-08-23 17:20:59 -0500

    Change of bootstrap theme

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import dash_bootstrap_components as dbc
 import dash
 
 # meta_tags are required for the app layout to be mobile responsive
+
 app = dash.Dash(__name__, 
-                external_stylesheets=[dbc.themes.BOOTSTRAP],
+                external_stylesheets=[dbc.themes.YETI],
                 suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0'}]
                 )
