1d1c5e4ed426cf8acb487857f767550fbc5d0ea6
namonroyr <namonroyr@unal.edu.co>
2021-08-24 03:29:43 -0500

    Title Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 
 # meta_tags are required for the app layout to be mobile responsive
 
-app = dash.Dash(__name__, title='KIDlytics',
+app = dash.Dash(__name__, title='ICBF',
                 external_stylesheets=[dbc.themes.YETI],
                 suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0'}]
