16495161c5ff4a6c9e658f6ff9f4ef527aedfd7d
namonroyr <namonroyr@unal.edu.co>
2021-08-24 17:57:23 -0500

    Header and sidebar update

--- apps/form.py
+++ apps/form.py
@@ -1,10 +1,36 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
 from dash.dependencies import Input, Output
 import plotly.express as px
+import pathlib
+import joblib
+from utils import SHAPValues
 from app import app
 
 
-layout = html.Div([
-    html.H1("Form goes here!!!!")
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../datasets").resolve()
+base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
+modelo_malnutrition = joblib.load('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav')
+
+shap_values_fig = SHAPValues.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
+
+layout = dbc.Container([
+    dbc.Row([
+    html.Img(src='/assets/long_child.jpg', height="200px"),
+    ], align="center",
+    ),
+    dbc.Row([
+        dcc.Graph(
+                    id = 'SHAPValues_plot',
+                    figure = shap_values_fig,
+                )
+    ]),
+
+    dbc.Row([
+
+    ]),
 ])
