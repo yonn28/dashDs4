0847229bcbade127d5c509e0a93275096af8b9de
namonroyr <namonroyr@unal.edu.co>
2021-08-24 17:28:55 -0500

    Revert "Sidebar and Header update"
    This reverts commit d0bad57c05d22ccda49a8437d36b5c2650a4f568.
    
    # Conflicts:
    #	apps/Malnutrition.py
    #	main.py

--- main.py
+++ main.py
@@ -2,57 +2,42 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
-# import dash_auth
+import dash_auth
 from app import app
 
 # Connect to app pages
-from apps import dashboard, Malnutrition, Relapse, PredicTool
+from apps import dashboard, form
 
-# USERNAMEINFO = [['user','password']]
-# auth = dash_auth.BasicAuth(app,USERNAMEINFO)
+#USERNAMEINFO = [['user','password']]
+#auth = dash_auth.BasicAuth(app,USERNAMEINFO)
 
-navbar = dbc.Navbar(
-    [
-        html.A(
-            dbc.Row(
-                [
-                    dbc.Col(dbc.NavbarBrand("ICBF - Children Malnutrition in Colombia", className="font-weight-bold"),
-                            align="left")
-                ],
-                align="center",
-            ),
-        ),
-        dbc.Collapse(
-            html.Img(src='/assets/correlation.png', height="50px", className="ml-auto flex-nowrap mt-3 mt-md-0"),
-            id="navbar-collapse", navbar=True, is_open=False
-        ),
-    ],
-    color="white",
-    light=True,
-)
 
-sidebar = html.Div(
+header = html.Div([
+    html.Img(src='/assets/correlation.png')
+], className="app_header")
+
+sidebar =  html.Div(
     [
         dbc.Nav(
             [
                 html.Hr(),
                 html.P("ANALYTICS"),
-                dbc.NavLink('Dashboard', href='/apps/dashboard', active="exact"),
+                dbc.NavLink('Dashboard', href='/apps/Dashboard', active="exact"),
                 html.P("CHILDREN AT RISK"),
                 dbc.NavLink('Malnutrition', href='/apps/Malnutrition', active="exact"),
                 dbc.NavLink('Relapse', href='/apps/Relapse', active="exact"),
                 dbc.NavLink('Prediction Tool', href='/apps/PredicTool', active="exact"),
-                # dbc.NavLink('About Us', href='/apps/About', active="exact"),
+                dbc.NavLink('About Us', href='/apps/About', active="exact"),
             ], className='nav-bar__links',
-            vertical=True,
-            pills=True,
+            vertical = True,
+            pills = True,
         ),
         html.Div(
             [
                 html.Img(src='/assets/bienestar.png'),
                 html.Img(src='/assets/team.png')
             ],
-            className="img_footer")
+        className="img_footer")
     ], className='app_side_bar'
 )
 
@@ -60,25 +45,25 @@ app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     sidebar,
     html.Div([
-        navbar,
-        html.Div(id='page-content', children=[], className='content')
+        header,
+        html.Div(id='page-content', children=[],className='content')
     ], className='app-container__header__content')
-
+    
 ], className='rooter_container')
-
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/apps/Dashboard':
-        return Dashboard.layout
+        return dashboard.layout
     if pathname == '/apps/Malnutrition':
-        return Malnutrition.layout
+        return form.layout
     else:
-        return Dashboard.layout
+        return dashboard.layout
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False, host="0.0.0.0", port=8080)
+
 
 
