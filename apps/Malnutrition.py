d0bad57c05d22ccda49a8437d36b5c2650a4f568
namonroyr <namonroyr@unal.edu.co>
2021-08-24 04:53:55 -0500

    Sidebar and Header update

---
+++ apps/Malnutrition.py
@@ -0,0 +1,63 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pathlib
+import joblib
+from utils import SHAPValues
+from app import app
+
+"""
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../datasets").resolve()
+base_final = pd.read_csv('/datasets/base_malnutrition.csv')
+"""
+
+'''
+2. Parámetros de corrida
+'''
+
+#0. Path a las base:
+#pathBaseRelapse = "./datasets/base_relapse.csv"
+pathBaseMalnutrition = "datasets/base_malnutrition.csv"
+
+# 1. Path a los modelos:
+#pathModeloRelapse = "./models/Modelo_relapse.sav"
+pathModeloMalnutrition = "models/Modelo_malnutrition.sav"
+
+'''
+3. Cargar la base:
+'''
+
+#base_relapse = pd.read_csv(pathBaseRelapse).drop(["IdBeneficiario","Unnamed: 0"],axis=1)
+base_malnutrition = pd.read_csv(pathBaseMalnutrition).drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
+
+
+'''
+4. Cargar los modelos:
+'''
+
+#modelo_relapse = joblib.load(pathModeloRelapse)
+modelo_malnutrition = joblib.load(pathModeloMalnutrition)
+
+shap_values_fig = SHAPValues.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
+
+layout = dbc.Container([
+    dbc.Row([
+    html.Img(src='/assets/long_child.jpg', height="200px"),
+    ], align="center",
+    ),
+    dbc.Row([
+        dcc.Graph(
+                    id = 'SHAPValues_plot', 
+                    figure = shap_values_fig,
+                )
+    ]),
+
+    dbc.Row([
+
+    ]),
+])
--- main.py
+++ main.py
@@ -2,21 +2,36 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
-import dash_auth
+# import dash_auth
 from app import app
 
 # Connect to app pages
-from apps import dashboard, form
+from apps import Malnutrition, Relapse, PredicTool
 
-#USERNAMEINFO = [['user','password']]
-#auth = dash_auth.BasicAuth(app,USERNAMEINFO)
+# USERNAMEINFO = [['user','password']]
+# auth = dash_auth.BasicAuth(app,USERNAMEINFO)
 
+navbar = dbc.Navbar(
+    [
+        html.A(
+            dbc.Row(
+                [
+                    dbc.Col(dbc.NavbarBrand("ICBF - Children Malnutrition in Colombia", className="font-weight-bold"),
+                            align="left")
+                ],
+                align="center",
+            ),
+        ),
+        dbc.Collapse(
+            html.Img(src='/assets/correlation.png', height="50px", className="ml-auto flex-nowrap mt-3 mt-md-0"),
+            id="navbar-collapse", navbar=True, is_open=False
+        ),
+    ],
+    color="white",
+    light=True,
+)
 
-header = html.Div([
-    html.Img(src='/assets/correlation.png')
-], className="app_header")
-
-sidebar =  html.Div(
+sidebar = html.Div(
     [
         dbc.Nav(
             [
@@ -27,17 +42,17 @@ sidebar =  html.Div(
                 dbc.NavLink('Malnutrition', href='/apps/Malnutrition', active="exact"),
                 dbc.NavLink('Relapse', href='/apps/Relapse', active="exact"),
                 dbc.NavLink('Prediction Tool', href='/apps/PredicTool', active="exact"),
-                dbc.NavLink('About Us', href='/apps/About', active="exact"),
+                # dbc.NavLink('About Us', href='/apps/About', active="exact"),
             ], className='nav-bar__links',
-            vertical = True,
-            pills = True,
+            vertical=True,
+            pills=True,
         ),
         html.Div(
             [
                 html.Img(src='/assets/bienestar.png'),
                 html.Img(src='/assets/team.png')
             ],
-        className="img_footer")
+            className="img_footer")
     ], className='app_side_bar'
 )
 
@@ -45,25 +60,25 @@ app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     sidebar,
     html.Div([
-        header,
-        html.Div(id='page-content', children=[],className='content')
+        navbar,
+        html.Div(id='page-content', children=[], className='content')
     ], className='app-container__header__content')
-    
+
 ], className='rooter_container')
+
 
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/apps/Dashboard':
-        return dashboard.layout
+        return Dashboard.layout
     if pathname == '/apps/Malnutrition':
-        return form.layout
+        return Malnutrition.layout
     else:
-        return dashboard.layout
+        return Dashboard.layout
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
-
+    app.run_server(debug=True)
 
 
