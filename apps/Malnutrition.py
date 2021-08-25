50ddefcea66a7854b7a955a756d2541746eba42a
namonroyr <namonroyr@unal.edu.co>
2021-08-25 01:49:57 -0500

    Rev - Intento de cargar el modelo 2

--- apps/Malnutrition.py
+++ apps/Malnutrition.py
@@ -7,12 +7,13 @@ import pandas as pd
 import shap # package used to calculate Shap values
 from app import app
 from urllib.request import urlopen
-import joblib # Cargar y guardar modelos sklearn
+import joblib
+# Cargar y guardar modelos sklearn
 #from utils import SHAPValues
 
 modelo_malnutrition_path = './assets/models/Modelo_malnutrition.sav'
 #with urlopen('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav') as response:
-modelo_malnutrition = joblib.load(modelo_malnutrition_path)
+#modelo_malnutrition = joblib.load(modelo_malnutrition_path)
 
 base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
 
