66aeb7a2ddbb08cf8dc257dba6654ab6c4a4c9dd
namonroyr <namonroyr@unal.edu.co>
2021-08-25 17:05:21 -0500

    Intento de cargar el modelo 12

--- apps/Malnutrition.py
+++ apps/Malnutrition.py
@@ -10,6 +10,18 @@ from app import app
 from urllib.request import urlopen
 from joblib import load
 import pathlib
+import tensorflow as tf
+
+"""
+MODEL_DIR = os.environ["MODEL_DIR"]
+MODEL_FILE_MALN = os.environ["MODEL_FILE_MALN"]
+MODEL_PATH_MALN = os.path.join(MODEL_DIR, MODEL_FILE_MALN)
+"""
+
+gcs_path = 'https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav'
+
+modelo_malnutrition = load(tf.io.gfile.GFile(MODEL_PATH_MALN, 'rb'))
+
 #from utils import SHAPVal
 """
 from io import BytesIO
@@ -23,15 +35,11 @@ modelo_malnutrition = pickle.load(mfile)
 #PATH = pathlib.Path(__file__).parent
 #DATA_PATH = PATH.joinpath("../assets/models").resolve()
 
-MODEL_DIR = os.environ["MODEL_DIR"]
-MODEL_FILE_MALN = os.environ["MODEL_FILE_MALN"]
-MODEL_PATH_MALN = os.path.join(MODEL_DIR, MODEL_FILE_MALN)
-
 
 #modelo_malnutrition_path = DATA_PATH+'Modelo_malnutrition.sav'
 #with urlopen('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav') as response:
 
-modelo_malnutrition = load(MODEL_PATH_MALN)
+#modelo_malnutrition = load(MODEL_PATH_MALN)
 
 base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
 #SHAP_Val.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
