efb70d583d3ebd598f9b26c3f78cfe3e531e7acf
namonroyr <namonroyr@unal.edu.co>
2021-08-25 13:49:27 -0500

    Intento de cargar el modelo 8

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY . ./
 
+ENV MODEL_DIR=/assests/models
+ENV MODEL_FILE_MALN=Modelo_malnutrition.sav
+ENV MODEL_FILE_RELP=Modelo_relapse.sav
 
 RUN python -m pip install -r requirements.txt
 
--- apps/Malnutrition.py
+++ apps/Malnutrition.py
@@ -8,7 +8,7 @@ import pandas as pd
 import shap # package used to calculate Shap values
 from app import app
 from urllib.request import urlopen
-import joblib
+from joblib import load
 import pathlib
 #from utils import SHAPVal
 """
@@ -23,12 +23,13 @@ modelo_malnutrition = pickle.load(mfile)
 #PATH = pathlib.Path(__file__).parent
 #DATA_PATH = PATH.joinpath("../assets/models").resolve()
 
-HOME_PATH = os.getcwd()
-MODELS_PATH = os.path.join(HOME_PATH, 'assets/models')
+MODEL_DIR = os.environ["MODEL_DIR"]
+MODEL_FILE_MALN = os.environ["MODEL_FILE_MALN"]
+MODEL_PATH_MALN = os.path.join(MODEL_DIR, MODEL_FILE_MALN)
 
 #modelo_malnutrition_path = DATA_PATH+'Modelo_malnutrition.sav'
 #with urlopen('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav') as response:
-modelo_malnutrition = joblib.load(os.path.join(MODELS_PATH, 'Modelo_malnutrition.sav'))
+modelo_malnutrition = load(os.path.join(MODEL_PATH_MALN))
 
 base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
 #SHAP_Val.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
