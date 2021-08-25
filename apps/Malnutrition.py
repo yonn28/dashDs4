932b32264cfd520479738b63b0b7aa04bcdccf33
namonroyr <namonroyr@unal.edu.co>
2021-08-25 00:08:34 -0500

    Intento de cargar el modelo a través de assets

--- apps/Malnutrition.py
+++ apps/Malnutrition.py
@@ -9,9 +9,9 @@ from app import app
 from urllib.request import urlopen
 import joblib # Cargar y guardar modelos sklearn
 
-
+modelo_malnutrition_path = '/assets/models/Modelo_malnutrition.sav'
 #with urlopen('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav') as response:
-#    modelo_malnutrition = joblib.load(response)
+modelo_malnutrition = joblib.load(modelo_malnutrition_path)
 
 base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
 
