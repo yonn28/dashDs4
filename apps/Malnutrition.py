e7edbe677509d7d8bb80fb9819afb41534c0db46
namonroyr <namonroyr@unal.edu.co>
2021-08-25 11:43:43 -0500

    Intento de cargar el modelo 7

--- apps/Malnutrition.py
+++ apps/Malnutrition.py
@@ -1,3 +1,4 @@
+import os
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -8,6 +9,7 @@ import shap # package used to calculate Shap values
 from app import app
 from urllib.request import urlopen
 import joblib
+import pathlib
 #from utils import SHAPVal
 """
 from io import BytesIO
@@ -17,9 +19,16 @@ mLink = 'https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav'
 mfile = BytesIO(requests.get(mLink).content)
 modelo_malnutrition = pickle.load(mfile)
 """
-modelo_malnutrition_path = './assets/models/Modelo_malnutrition.sav'
+
+#PATH = pathlib.Path(__file__).parent
+#DATA_PATH = PATH.joinpath("../assets/models").resolve()
+
+HOME_PATH = os.getcwd()
+MODELS_PATH = os.path.join(HOME_PATH, 'assets/models')
+
+#modelo_malnutrition_path = DATA_PATH+'Modelo_malnutrition.sav'
 #with urlopen('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition.sav') as response:
-modelo_malnutrition = joblib.load(modelo_malnutrition_path)
+modelo_malnutrition = joblib.load(os.path.join(MODELS_PATH, 'Modelo_malnutrition.sav'))
 
 base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
 #SHAP_Val.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
