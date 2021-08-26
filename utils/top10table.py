764fb4a1297a12878f8a6e346c8da73b78696fc5
namonroyr <namonroyr@unal.edu.co>
2021-08-26 12:30:48 -0500

    top10table

--- utils/top10table.py
+++ utils/top10table.py
@@ -1,11 +1,26 @@
 import pandas as pd
-
-# Cargar y guardar modelos sklearn:
-
-import joblib
-
 # Visualization:
-
-import plotly.graph_objects as go
 import plotly.io as pio
-import matplotlib.pyplot as plt
+
+pio.renderers.default = 'browser'
+
+
+def createTable_top(objeto_modelo, base_variables):
+    # Create a new column with the predicted probability:
+
+    base_variables.loc[:, "Probability"] = objeto_modelo.predict_proba(base_variables)[:, 1]
+    base_variables = base_variables.sort_values("Probability", ascending=False)
+
+    # Create a column with the range of the probability:
+
+    base_variables.loc[:, "Range_probability"] = pd.qcut(base_variables['Probability'], q=10, precision=0,
+                                                         duplicates='drop')
+
+    # Top 10%
+
+    a = pd.DataFrame(base_variables.groupby(["Range_probability"]).size().reset_index()).rename(columns={0: 'Total'})
+    buscado = a.loc[9, "Range_probability"]
+
+    c_df = base_variables[base_variables["Range_probability"] == buscado]
+
+    return (c_df)
