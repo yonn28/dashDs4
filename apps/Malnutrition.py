f9e13e71eb8f272d1214103f6453679226ce374f
namonroyr <namonroyr@unal.edu.co>
2021-08-26 12:51:41 -0500

    correction

--- apps/Malnutrition.py
+++ apps/Malnutrition.py
@@ -5,7 +5,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 import shap # package used to calculate Shap values
-from utils import SHAP_Val
+#from utils import SHAP_Val
 from app import app
 from urllib.request import urlopen
 import joblib
@@ -18,7 +18,8 @@ with urlopen('https://storage.googleapis.com/ds4all-test-bd1/Modelo_malnutrition
     modelo_malnutrition = joblib.load(response)
 
 base_malnutrition = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/base_malnutrition.csv').drop(["IdBeneficiario","Unnamed: 0","Unnamed: 0.1"],axis=1)
-SHAP_Val.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
+
+#SHAP_Val.plotShapValuesTop(modelo_malnutrition, base_malnutrition)
 
 layout = dbc.Container([
     dbc.Row([
