698b1c574264a661241bb414e39bcd8c93a5bc0b
yonny <ynova@bancodebogota.com.co>
2021-08-27 21:32:36 -0500

    changing to micro services

--- utils/plot_by_year.py
+++ utils/plot_by_year.py
@@ -1,31 +1,16 @@
 import pandas as pd
 import plotly.express as px
 from itertools import cycle
+import requests
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
-base_pivot = pd.read_csv('https://storage.googleapis.com/ds4all-test-bd1/tomas_pivot.csv')
-base_pivot = base_pivot.drop(columns = ['Unnamed: 0'])
-
-def nombres(x):
-    if x == 'Desnutricion Aguda Moderada' or x == 'Desnutricion aguda severa':
-        x = 'Desnutricion'
-    else:
-        x
-    return x
-
-base_pivot['EstadoPesoTalla_New'] = base_pivot['EstadoPesoTalla'].apply(nombres)
-base_pivot['FechaValoracionNutricional'] = pd.to_datetime(base_pivot['FechaValoracionNutricional'])
-base_pivot['FechaValoracionNutricional_MesAño'] = base_pivot['FechaValoracionNutricional'].dt.to_period('M')
-frecuencias_mes = base_pivot[['FechaValoracionNutricional_MesAño','EstadoPesoTalla_New']].groupby(['FechaValoracionNutricional_MesAño','EstadoPesoTalla_New']).size()
-porcentajes_mes = frecuencias_mes.groupby(level=0).apply(lambda x : 100 * x / float(x.sum()))
-porcentajes_mes = porcentajes_mes.reset_index()
-porcentajes_mes.columns = ['FechaValoracionNutricional_MesAño', 'EstadoPesoTalla_New', 'Porcentajes']
 
 def ploting_distribution():
-    x = porcentajes_mes['FechaValoracionNutricional_MesAño'].unique().strftime('%b')[:12]
-    y = porcentajes_mes[porcentajes_mes['EstadoPesoTalla_New'] == 'Desnutricion']['Porcentajes']
+    ry = requests.get('https://microservice1-zbca65qbuq-nn.a.run.app/api/v1/y')
+    y = ry.json()
+    rx = requests.get('https://microservice1-zbca65qbuq-nn.a.run.app/api/v1/x')
+    porcentajes_mes_dict = requests.get('https://microservice1-zbca65qbuq-nn.a.run.app/api/v1/df')
+    porcentajes_mes=pd.DataFrame.from_dict(porcentajes_mes_dict.json())
+    x = rx.json()
     y1 = y[:12]
     y2 = y[12:24]
     y3 = y[24:]
