ae08ca5828c96396d123ddca0da3a71aae8ad3e1
yonny <ynova@bancodebogota.com.co>
2021-08-30 20:27:07 -0500

    solving style issues

--- apps/About.py
+++ apps/About.py
@@ -24,7 +24,7 @@ layout = html.Div(
             [
                 dbc.Col(
                     [   
-                        html.Img(src='/assets/pics/nicolas.png', height="250px",id="nicolas_photo",style={"display":"block","margin-right":"auto","margin-left":"auto"}),
+                        html.Img(src='/assets/pics/nicolas.png', height="250px",id="nicolas_photo"),
                         html.Div("\t Nicolás Cabrera Malik",style={'color': 'blue', 'fontSize': 20,"text-align": "center"}),
                         html.Div("\t PhD Student",style={'color': 'darkblue', 'fontSize': 15,"text-align": "center"}),
                         html.A([
@@ -41,7 +41,7 @@ layout = html.Div(
                     ),
                 dbc.Col(
                     [
-                        html.Img(src='/assets/pics/julian.png', height="250px",id="julian_photo",style={"display":"block","margin-right":"auto","margin-left":"auto"}),
+                        html.Img(src='/assets/pics/julian.png', height="250px",id="julian_photo"),
                         html.Div("\t Julian Monsalve",style={'color': 'blue', 'fontSize': 20,"text-align": "center"}),
                         html.Div("\t Industrial Engineer",style={'color': 'darkblue', 'fontSize': 15,"text-align": "center"}),
                         html.A([
@@ -58,7 +58,7 @@ layout = html.Div(
                     ),
                 dbc.Col(
                     [
-                        html.Img(src='/assets/pics/david.png', height="250px",id="david_photo",style={"display":"block","margin-right":"auto","margin-left":"auto"}),
+                        html.Img(src='/assets/pics/david.png', height="250px",id="david_photo"),
                         html.Div("\t David Quintero",style={'color': 'blue', 'fontSize': 20,"text-align": "center"}),
                         html.Div("\t Electronic Engineer",style={'color': 'darkblue', 'fontSize': 15,"text-align": "center"}),
                         html.A([
@@ -79,7 +79,7 @@ layout = html.Div(
             [
                 dbc.Col(
                     [   
-                        html.Img(src='/assets/pics/natalia.png', height="250px",id="natalia_photo",style={"display":"block","margin-right":"auto","margin-left":"auto"}),
+                        html.Img(src='/assets/pics/natalia.png', height="250px",id="natalia_photo"),
                         html.Div("\t Natalia Monroy",style={'color': 'blue', 'fontSize': 20,"text-align": "center"}),
                         html.Div("\t Computer Science Student",style={'color': 'darkblue', 'fontSize': 15,"text-align": "center"}),
                         html.A([
@@ -96,7 +96,7 @@ layout = html.Div(
                     ),
                 dbc.Col(
                     [
-                        html.Img(src='/assets/pics/Daniel.png', height="250px",id="daniel_photo",style={"display":"block","margin-right":"auto","margin-left":"auto"}),
+                        html.Img(src='/assets/pics/Daniel.png', height="250px",id="daniel_photo"),
                         html.Div("\t Daniel Ramirez",style={'color': 'blue', 'fontSize': 20,"text-align": "center"}),
                         html.Div("\t Industrial Engineer",style={'color': 'darkblue', 'fontSize': 15,"text-align": "center"}),
                         html.A([
@@ -113,7 +113,7 @@ layout = html.Div(
                     ),
                 dbc.Col(
                     [
-                        html.Img(src='/assets/pics/Yonny.png', height="250px",id="yonny_photo",style={"display":"block","margin-right":"auto","margin-left":"auto"}),
+                        html.Img(src='/assets/pics/Yonny.png', height="250px",id="yonny_photo"),
                         html.Div("\t Yonny Nova",style={'color': 'blue', 'fontSize': 20,"text-align": "center"}),
                         html.Div("\t Electronic Engineer",style={'color': 'darkblue', 'fontSize': 15,"text-align": "center"}),
                         html.A([
