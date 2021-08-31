32517165ea3766ebf282d75e9701bbcd5e255ac5
yonny <ynova@bancodebogota.com.co>
2021-08-30 20:06:34 -0500

    deleting coments

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ sidebar = html.Div(
                 dbc.NavLink("Home", href='/apps/Home', active="exact"),
                 html.Hr(),
                 html.P("ANALYTICS", className='text-p'),
-                #dbc.NavLink('Dashboard', href='/apps/Dashboard', active="exact"),
+                dbc.NavLink('Dashboard', href='/apps/Dashboard', active="exact"),
                 html.P("CHILDREN AT RISK", className='text-p'),
-                #dbc.NavLink('Malnutrition', href='/apps/Malnutrition', active="exact"),
-                #dbc.NavLink('Relapse', href='/apps/Relapse', active="exact"),
+                dbc.NavLink('Malnutrition', href='/apps/Malnutrition', active="exact"),
+                dbc.NavLink('Relapse', href='/apps/Relapse', active="exact"),
                 dbc.NavLink('Prediction Tool', href='/apps/PredicTool', active="exact"),
                 dbc.NavLink('About Us', href='/apps/About', active="exact"),
             ],
@@ -77,16 +77,16 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/apps/Home':
         return Home.layout
-    #if pathname == '/apps/Dashboard':
-        #return dashboard.layout
-    #if pathname == '/apps/Malnutrition':
-        #return Malnutrition.layout
+    if pathname == '/apps/Dashboard':
+        return dashboard.layout
+    if pathname == '/apps/Malnutrition':
+        return Malnutrition.layout
     if pathname == '/apps/PredicTool':
         return PredicTool.layout
     if pathname == '/apps/About':
         return About.layout
-    #else:
-        #return dashboard.layout
+    else:
+        return dashboard.layout
 
 
 if __name__ == '__main__':
